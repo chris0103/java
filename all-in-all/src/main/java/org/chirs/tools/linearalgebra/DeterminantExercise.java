e42f8e4383376fbf0a5e2074548644f935bf18ce
chris0103 <chris0103@126.com>
2014-11-28 17:05:16 +0800

    Add tools for linear algegra computations.

---
+++ all-in-all/src/main/java/org/chirs/tools/linearalgebra/DeterminantExercise.java
@@ -0,0 +1,73 @@
+package org.chirs.tools.linearalgebra;
+
+import java.util.Random;
+
+import linearalgebra.number.Fraction;
+
+public class DeterminantExercise {
+
+	private static final int LEVEL = 3;
+
+	private static Random random = new Random();
+	
+	private Fraction[][] det = new Fraction[LEVEL][LEVEL];
+	
+	public void generate() {
+		for (int m = 0; m < LEVEL; m++) {
+			for (int n = 0; n < LEVEL; n++) {
+				int num = random.nextInt(100) - random.nextInt(100);
+				int den = 1;
+				while (den == 0) {
+					den = random.nextInt(100) - random.nextInt(100);
+				}
+				det[m][n] = new Fraction(num, den);
+			}
+		}
+	}
+	
+	public void print() {
+		for (int m = 0; m < LEVEL; m++) {
+			for (int n = 0; n < LEVEL; n++) {
+				System.out.print(det[m][n]+ "\t\t");
+			}
+			System.out.println();
+		}
+	}
+	
+	public Fraction calculate() {
+		return calculate(det);
+	}
+	
+	private Fraction calculate(Fraction[][] det) {
+		int len = det.length;
+		if (len == 2) {
+			return det[0][0].multiply(det[1][1]).substract(det[0][1].multiply(det[1][0]));
+		}
+		Fraction sum = new Fraction(0, 1);
+		for (int n = 0; n < len; n++) {
+			Fraction a = det[0][n];
+			if ((n + 1) % 2 == 1) {
+				a = a.negate();
+			}
+			Fraction[][] subDet = new Fraction[len - 1][len - 1];
+			for (int mm = 1; mm < len; mm++) {
+				int cc = 0;
+				for (int nn = 0; nn < len; nn++) {
+					if (nn != n) {
+						subDet[mm - 1][cc++] = det[mm - 1][nn];
+					}
+				}
+			}
+			sum.add(calculate(subDet));
+		}
+		return sum;
+	}
+	
+	public static void main(String[] args) {
+		DeterminantExercise ex = new DeterminantExercise();
+		ex.generate();
+		ex.print();
+		Fraction result = ex.calculate();
+		System.out.println(result);
+	}
+}
---
+++ all-in-all/src/main/java/org/chirs/tools/linearalgebra/MatrixTransformer.java
@@ -0,0 +1,87 @@
+package org.chirs.tools.linearalgebra;
+
+public class MatrixTransformer {
+
+	public static final int ROW = 1;
+	public static final int COLUMN = 2;
+	
+	private boolean verbose = false;
+	
+	public MatrixTransformer() {
+		
+	}
+	
+	public MatrixTransformer(boolean verbose) {
+		this.verbose = verbose;
+	}
+	
+	public void shift(int[][] matrix, int dimension, int i, int j) {
+		if (dimension == ROW) {
+			int n = matrix[0].length;
+			int[] temp = new int[n];
+			for (int k = 0; k < n; k++) {
+				temp[k] = matrix[i][k];
+				matrix[i][k] = matrix[j][k];
+				matrix[j][k] = temp[k];
+			}
+		} else if (dimension == COLUMN) {
+			int m = matrix.length;
+			int[] temp = new int[m];
+			for (int k = 0; k < m; k++) {
+				temp[k] = matrix[k][i];
+				matrix[k][i] = matrix[k][j];
+				matrix[k][j] = temp[k];
+			}
+		}
+		if (verbose) {
+			print(matrix);
+		}
+	}
+	
+	public void augment(int[][] matrix, int dimension, int i, int j, int k) {
+		if (dimension == ROW) {
+			int n = matrix[0].length;
+			for (int r = 0; r < n; r++) {
+				matrix[i][r] += matrix[j][r] * k;
+			}
+		} else if (dimension == COLUMN) {
+			int m = matrix.length;
+			for (int r = 0; r < m; r++) {
+				matrix[r][i] += matrix[r][j] * k;
+			}
+		}
+		if (verbose) {
+			print(matrix);
+		}
+	}
+	
+	public void multiply(int[][] matrix, int dimension, int i, int k) {
+		augment(matrix, dimension, i, i, k);
+	}
+	
+	public void print(int[][] matrix) {
+		for (int[] row : matrix) {
+			for (int data : row) {
+				System.out.print(data + "\t");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[][] matrix = {
+			{2, 0, 1, 1, 0, 0}, {-1, -1, -2, 0, 1, 0}, {-3, 0, 1, 0, 0, 1}	
+		};
+		MatrixTransformer mt = new MatrixTransformer(true);
+		mt.shift(matrix, ROW, 1, 2);
+		mt.multiply(matrix, ROW, 1, -1);
+		// mt.shift(matrix, ROW, 2, 1);
+		// mt.shift(matrix, ROW, 1, 0);
+		// mt.print(matrix);
+		// System.out.println();
+		// mt.augment(matrix, ROW, 1, 0, -3);
+		// mt.augment(matrix, ROW, 2, 0, -3);
+		// mt.augment(matrix, ROW, 3, 0, -2);
+		// mt.print(matrix);
+	}
+}
