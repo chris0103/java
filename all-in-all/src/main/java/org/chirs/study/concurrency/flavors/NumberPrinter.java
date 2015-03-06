00654385260f54dc611af6da5fcc10e75fb61f54
chris0103 <chris0103@126.com>
2015-03-06 15:17:22 +0800

    Revert "Overhaul of the concurrency study."
    This reverts commit c1e2bad3aed2fe98502858ff04fdafaa54a2e415.

---
+++ all-in-all/src/main/java/org/chirs/study/concurrency/flavors/NumberPrinter.java
@@ -0,0 +1,18 @@
+package org.chirs.study.concurrency.flavors;
+
+import java.util.List;
+
+public interface NumberPrinter {
+
+	/**
+	 * Gets the print name.
+	 * @return
+	 */
+	public String getPrinterName();
+	
+	/**
+	 * Prints a list of numbers and returns the sum.
+	 * @param num
+	 */
+	public int toNumber(List<Integer> nums);
+}
