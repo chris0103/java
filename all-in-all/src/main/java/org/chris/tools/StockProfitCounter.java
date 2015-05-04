b6124de7336df242e081322be9658f2fb179ab40
Youjing Zhu <chris0103@126.com>
2015-05-04 21:23:56 +0800

    Update stock profit counter.
    Update stock profit counter.

--- all-in-all/src/main/java/org/chris/tools/StockProfitCounter.java
+++ all-in-all/src/main/java/org/chris/tools/StockProfitCounter.java
@@ -12,7 +12,7 @@ public class StockProfitCounter {
 	private static final String S600645 = "中原协和 (600645)"; 
 	private static final String S600004 = "白云机场 (600004)"; 
 	private static final String S300188 = "美亚柏科 (300188)";
-	private static final String S000014 = "沙河股份 (000014)";
+	private static final String S600005 = "武钢股份 (600005)";
 	private static final String S600000 = "浦发银行 (600000)";
 
 	
@@ -21,13 +21,13 @@ public class StockProfitCounter {
 	private static DecimalFormat decFormat = new DecimalFormat("####.##");
 	
 	public StockProfitCounter() {
-		stocks.put(S600340, new double[] {47.430, 47.430});
+		stocks.put(S600340, new double[] {57.652, 57.652, 57.652, 57.652});
 		stocks.put(S000018, new double[] {25.646, 25.646, 25.646, 25.646});
-		stocks.put(S600645, new double[] {58.142});
-		stocks.put(S600004, new double[] {11.111, 11.111});
-		stocks.put(S300188, new double[] {35.222, 35.222, 35.222, 35.222});
-		// stocks.put(S000014, new double[] {});
-		stocks.put(S600000, new double[] {14.167, 14.167, 14.875, 14.875});
+		stocks.put(S600645, new double[] {64.102});
+		stocks.put(S600004, new double[] {12.250, 12.250, 12.862, 12.862});
+		stocks.put(S300188, new double[] {44.952, 44.952, 44.952, 44.952});
+		stocks.put(S600005, new double[] {4.995, 4.995, 4.995, 4.995});
+		// stocks.put(S600000, new double[] {});
 	}
 
 	public void countProfilt(Map<String, Double> curPrices) {
@@ -49,13 +49,13 @@ public class StockProfitCounter {
 	
 	public static void main(String[] args) {
 		Map<String, Double> curPrices = new HashMap<String, Double>();
-		curPrices.put(S600340, 44.72);	// 华夏幸福 (600340)
-		curPrices.put(S000018, 26.10);	// 中冠A (000018)
-		curPrices.put(S600645, 61.04);	// 中原协和 (600645)
-		curPrices.put(S600004, 11.34);	// 白云机场 (600004)
-		curPrices.put(S300188, 35.62);	// 美亚柏科 (300188)
-		// curPrices.put(S000014, 13.38);	// 沙河股份 (000014)
-		curPrices.put(S600000, 14.10);	// 浦发银行 (600000)
+		curPrices.put(S600340, 55.71);	// 华夏幸福 (600340)
+		curPrices.put(S000018, 26.67);	// 中冠A (000018)
+		curPrices.put(S600645, 64.86);	// 中原协和 (600645)
+		curPrices.put(S600004, 13.41);	// 白云机场 (600004)
+		curPrices.put(S300188, 44.71);	// 美亚柏科 (300188)
+		curPrices.put(S600005, 5.09);	// 武钢股份 (600005)
+		// curPrices.put(S600005, 14.10);	// 浦发银行 (600000)
 		StockProfitCounter counter = new StockProfitCounter();
 		counter.countProfilt(curPrices);
 	}
