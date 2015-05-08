9b95d8d7db4955e1c241fd8dff25c277bf9d3354
chris0103 <chris0103@126.com>
2015-05-08 15:54:12 +0800

    Update stock profit counter.

--- all-in-all/src/main/java/org/chris/tools/StockProfitCounter.java
+++ all-in-all/src/main/java/org/chris/tools/StockProfitCounter.java
@@ -10,10 +10,10 @@ public class StockProfitCounter {
 	private static final String S600340 = "华夏幸福 (600340)";
 	private static final String S000018 = "中冠A (000018)";
 	private static final String S600645 = "中原协和 (600645)"; 
-	private static final String S600004 = "白云机场 (600004)"; 
+	private static final String S600021 = "上海电力 (600021)"; 
 	private static final String S300188 = "美亚柏科 (300188)";
 	private static final String S600005 = "武钢股份 (600005)";
-	private static final String S600000 = "浦发银行 (600000)";
+	private static final String S300002 = "神州泰岳 (300002)";
 
 	
 	private static Map<String, double[]> stocks = new LinkedHashMap<String, double[]>();
@@ -21,13 +21,13 @@ public class StockProfitCounter {
 	private static DecimalFormat decFormat = new DecimalFormat("####.##");
 	
 	public StockProfitCounter() {
-		stocks.put(S600340, new double[] {57.652, 57.652, 57.652, 57.652});
-		stocks.put(S000018, new double[] {25.646, 25.646, 25.646, 25.646});
-		stocks.put(S600645, new double[] {64.102});
-		stocks.put(S600004, new double[] {12.250, 12.250, 12.862, 12.862});
-		stocks.put(S300188, new double[] {44.952, 44.952, 44.952, 44.952});
-		stocks.put(S600005, new double[] {4.995, 4.995, 4.995, 4.995});
-		// stocks.put(S600000, new double[] {});
+		stocks.put(S600340, new double[] {28.827, 28.827, 31.782, 31.782});
+		stocks.put(S000018, new double[] {31.173, 31.173, 32.731, 32.731, 32.731, 32.731});
+		stocks.put(S600645, new double[] {77.917});
+		stocks.put(S600021, new double[] {23.645, 23.645, 23.645, 23.645, 24.827, 24.827, 24.827, 24.827, 26.069, 26.069, 26.069, 26.069});
+		stocks.put(S300188, new double[] {60.242, 60.242});
+		stocks.put(S600005, new double[] {5.509, 5.509, 6.074, 6.074});
+		stocks.put(S300002, new double[] {32.617, 32.617, 34.248, 34.248});
 	}
 
 	public void countProfilt(Map<String, Double> curPrices) {
@@ -49,13 +49,13 @@ public class StockProfitCounter {
 	
 	public static void main(String[] args) {
 		Map<String, Double> curPrices = new HashMap<String, Double>();
-		curPrices.put(S600340, 55.71);	// 华夏幸福 (600340)
-		curPrices.put(S000018, 26.67);	// 中冠A (000018)
-		curPrices.put(S600645, 64.86);	// 中原协和 (600645)
-		curPrices.put(S600004, 13.41);	// 白云机场 (600004)
-		curPrices.put(S300188, 44.71);	// 美亚柏科 (300188)
-		curPrices.put(S600005, 5.09);	// 武钢股份 (600005)
-		// curPrices.put(S600005, 14.10);	// 浦发银行 (600000)
+		curPrices.put(S600340, 29.59);	// 华夏幸福 (600340)
+		curPrices.put(S000018, 33.08);	// 中冠A (000018)
+		curPrices.put(S600645, 79.34);	// 中原协和 (600645)
+		curPrices.put(S600021, 23.81);	// 上海电力 (600021)
+		curPrices.put(S300188, 63.47);	// 美亚柏科 (300188)
+		curPrices.put(S600005, 5.75);	// 武钢股份 (600005)
+		curPrices.put(S300002, 33.89);	// 神州泰岳 (300002)
 		StockProfitCounter counter = new StockProfitCounter();
 		counter.countProfilt(curPrices);
 	}
