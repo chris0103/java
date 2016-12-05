a35955028201265653fb564c8c39e808da4db1c7
Youjing Zhu <chris0103@126.com>
2016-12-05 22:13:06 +0800

    Updated ReadingSchedule.

--- all-in-all/src/main/java/org/chris/tools/ReadingScheduler.java
+++ all-in-all/src/main/java/org/chris/tools/ReadingScheduler.java
@@ -5,7 +5,7 @@ import java.util.Calendar;
 
 public class ReadingScheduler {
 
-	private static SimpleDateFormat format = new SimpleDateFormat("MM-dd");
+	private static SimpleDateFormat format = new SimpleDateFormat("MM/dd");
 	
 	public void scheduleReading(String title, int start, int end, int quota) {
 		System.out.println("Reading schedule for " + title);
@@ -13,7 +13,7 @@ public class ReadingScheduler {
 		int days = Math.round(((float) pages) / ((float) quota));
 		int progress = start;
 		Calendar schedule = Calendar.getInstance();
-		// schedule.roll(Calendar.DAY_OF_YEAR, false);
+		schedule.roll(Calendar.DAY_OF_YEAR, false);
 		while (progress < end) {
 			schedule.roll(Calendar.DAY_OF_YEAR, true);
 			if (schedule.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY || schedule.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY) {
@@ -22,14 +22,15 @@ public class ReadingScheduler {
 			progress += quota;
 			if (progress > end) {
 				progress = end;
-			}
-			System.out.println(format.format(schedule.getTime()) + ":\t" + progress + "\t\t[ ]");
+			}			
+			// System.out.print(format.format(schedule.getTime()) + "\t");
+			System.out.println(progress);
 		}
 		System.out.println("Totally " + days + " days.");
 	}
 	
 	public static void main(String[] args) {
 		ReadingScheduler scheduler = new ReadingScheduler();
-		scheduler.scheduleReading("Spring in Action 4", 0, 626, 3);
+		scheduler.scheduleReading("Java Concurrency in Practice", 0, 425, 4);
 	}
 }
