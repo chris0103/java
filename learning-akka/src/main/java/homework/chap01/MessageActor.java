742d7a58ce062b3d6afacf01e72aec36b032d879
Youjing Zhu <chris0103@126.com>
2018-10-19 21:50:10 +0800

    Changed project name.

---
+++ learning-akka/src/main/java/homework/chap01/MessageActor.java
@@ -0,0 +1,19 @@
+package homework.chap01;
+
+import akka.actor.AbstractActor;
+import akka.actor.Status;
+import akka.japi.pf.ReceiveBuilder;
+import scala.PartialFunction;
+
+public class MessageActor extends AbstractActor {
+	
+	private String message;
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+	public PartialFunction receive() {
+		return ReceiveBuilder.match(String.class, str -> message = str)
+				.matchAny(x -> sender().tell(new Status.Failure(new Exception("unknown message")), self()))
+				.build();
+	}
+}
