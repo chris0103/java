9b88bd365f06e7f8f1696c98a09c988b9610bbbe
Youjing Zhu <chris0103@126.com>
2018-10-19 22:26:50 +0800

    Homework for chap01.

--- learning-akka/src/main/java/homework/chap01/MessageActor.java
+++ learning-akka/src/main/java/homework/chap01/MessageActor.java
@@ -6,14 +6,17 @@ import akka.japi.pf.ReceiveBuilder;
 import scala.PartialFunction;
 
 public class MessageActor extends AbstractActor {
-	
+
 	private String message;
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
 	public PartialFunction receive() {
 		return ReceiveBuilder.match(String.class, str -> message = str)
-				.matchAny(x -> sender().tell(new Status.Failure(new Exception("unknown message")), self()))
-				.build();
+				.matchAny(x -> sender().tell(new Status.Failure(new Exception("unknown message")), self())).build();
+	}
+	
+	public String getMessage() {
+		return message;
 	}
 }
