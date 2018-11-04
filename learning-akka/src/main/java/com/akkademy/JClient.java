d0d7677fe3e8ae2843a138b2fa57ce4cfd46cda8
Youjing Zhu <chris0103@126.com>
2018-11-04 23:15:06 +0800

    Updated learning files.

---
+++ learning-akka/src/main/java/com/akkademy/JClient.java
@@ -0,0 +1,31 @@
+package com.akkademy;
+
+import static akka.pattern.Patterns.ask;
+import static scala.compat.java8.FutureConverters.toJava;
+
+import java.util.concurrent.CompletionStage;
+
+import com.akkademy.messages.GetRequest;
+import com.akkademy.messages.SetRequest;
+
+import akka.actor.ActorSelection;
+import akka.actor.ActorSystem;
+
+public class JClient {
+	
+    private final ActorSystem system = ActorSystem.create("LocalSystem");
+    
+    private final ActorSelection remoteDb;
+
+    public JClient(String remoteAddress) {
+        remoteDb = system.actorSelection("akka.tcp://akkademy@" + remoteAddress + "/user/akkademy-db");
+    }
+
+    public CompletionStage<Object> set(String key, Object value) {
+        return toJava(ask(remoteDb, new SetRequest(key, value), 2000));
+    }
+
+    public CompletionStage<Object> get(String key) {
+        return toJava(ask(remoteDb, new GetRequest(key), 2000));
+    }
+}
---
+++ learning-akka/src/main/java/com/akkademy/Main.java
@@ -0,0 +1,15 @@
+package com.akkademy;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
+public class Main {
+	
+    public static void main(String... args) {
+        ActorSystem system = ActorSystem.create("akkademy");
+        
+		@SuppressWarnings("unused")
+		ActorRef actor = system.actorOf(Props.create(AkkademyDb.class), "akkademy-db");
+    }
+}
