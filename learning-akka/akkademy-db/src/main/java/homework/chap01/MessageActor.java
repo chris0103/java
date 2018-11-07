92ac17248a297942f80ae3ee0291b1002906f2ab
Youjing Zhu <chris0103@126.com>
2018-11-07 22:58:26 +0800

    Relocated and renamed learning-akka projects.

--- learning-akka/akkademy-db/src/main/java/homework/chap01/MessageActor.java
+++ learning-akka/akkademy-db/src/main/java/homework/chap01/MessageActor.java
@@ -4,14 +4,14 @@ import akka.actor.AbstractActor;
 import akka.actor.Status;
 import akka.japi.pf.ReceiveBuilder;
 import scala.PartialFunction;
+import scala.runtime.BoxedUnit;
 
 public class MessageActor extends AbstractActor {
 
 	private String message;
 
-	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
-	public PartialFunction receive() {
+	public PartialFunction<Object, BoxedUnit> receive() {
 		return ReceiveBuilder.match(String.class, str -> message = str)
 				.matchAny(x -> sender().tell(new Status.Failure(new Exception("unknown message")), self())).build();
 	}
---
+++ learning-akka/akkademy-db/src/main/java/homework/chap02/StringReverseActor.java
@@ -0,0 +1,25 @@
+package homework.chap02;
+
+import akka.actor.AbstractActor;
+import akka.actor.Status;
+import akka.japi.pf.ReceiveBuilder;
+import scala.PartialFunction;
+import scala.runtime.BoxedUnit;
+
+public class StringReverseActor extends AbstractActor {
+
+	@Override
+	public PartialFunction<Object, BoxedUnit> receive() {
+		return ReceiveBuilder.match(String.class, str -> reverse(str))
+				.matchAny(x -> sender().tell(new Status.Failure(new Exception("unknown message")), self())).build();
+	}
+
+	private String reverse(String str) {
+		if (str == null || str.isEmpty()) {
+			return str;
+		} else {
+			StringBuffer sb = new StringBuffer(str);
+			return sb.reverse().toString();
+		}
+	}
+}
