32597d15af31ac00b64254b89d76cc324493719b
Chris Zhu <CZhu30@its.jnj.com>
2019-01-04 17:44:28 +0800

    Added test method for failure case.

--- learning-akka/akkademy-db-client/src/main/java/com/akkademy/JClient.java
+++ learning-akka/akkademy-db-client/src/main/java/com/akkademy/JClient.java
@@ -12,20 +12,23 @@ import akka.actor.ActorSelection;
 import akka.actor.ActorSystem;
 
 public class JClient {
-	
+
     private final ActorSystem system = ActorSystem.create("LocalSystem");
-    
+
     private final ActorSelection remoteDb;
 
     public JClient(String remoteAddress) {
         remoteDb = system.actorSelection("akka.tcp://akkademy@" + remoteAddress + "/user/akkademy-db");
     }
 
-    public CompletionStage<Object> set(String key, Object value) {
-        return toJava(ask(remoteDb, new SetRequest(key, value), 2000));
+    public void disconnect() {
     }
 
     public CompletionStage<Object> get(String key) {
         return toJava(ask(remoteDb, new GetRequest(key), 2000));
+    }
+
+    public CompletionStage<Object> set(String key, Object value) {
+        return toJava(ask(remoteDb, new SetRequest(key, value), 2000));
     }
 }
--- learning-akka/akkademy-db-client/src/test/java/com/akkademy/JClientIntegrationTest.java
+++ learning-akka/akkademy-db-client/src/test/java/com/akkademy/JClientIntegrationTest.java
@@ -1,24 +1,51 @@
 package com.akkademy;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.concurrent.CompletableFuture;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import com.akkademy.messages.KeyNotFoundException;
 
 public class JClientIntegrationTest {
 
-    private final JClient client = new JClient("127.0.0.1:2552");
+    private JClient client = null;
+
+    @After
+    public void destroy() {
+        client.disconnect();
+    }
 
     @Test
     public void itShouldFailedOnMissingRecord() throws Exception {
         client.set("123", 123);
-        CompletableFuture<Object> future = (CompletableFuture<Object>) client.get("123");
-        System.out.println(future.get());
+        CompletableFuture<Object> future = (CompletableFuture<Object>) client.get("321").exceptionally(t -> {
+            if (t instanceof KeyNotFoundException) {
+                return constructErrorMessage((KeyNotFoundException) t);
+            }
+            return t.getMessage();
+        });
+        assertEquals(constructErrorMessage(new KeyNotFoundException("321")), future.get());
     }
 
+    @Ignore
     @Test
     public void itShouldSetRecord() throws Exception {
         client.set("123", 123);
         Integer result = (Integer) ((CompletableFuture<Object>) client.get("123")).get();
         assert (result == 123);
+    }
+
+    @Before
+    public void setup() {
+        client = new JClient("127.0.0.1:2552");
+    }
+
+    private String constructErrorMessage(KeyNotFoundException t) {
+        return t.getClass() + "[" + t.getKey() + "]";
     }
 }
