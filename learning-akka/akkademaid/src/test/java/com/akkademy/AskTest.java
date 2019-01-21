a77680ba3ab222be4347803a2e0de28ad25836ac
Chris Zhu <CZhu30@its.jnj.com>
2019-01-21 16:04:57 +0800

    Reformatting...

--- learning-akka/akkademaid/src/test/java/com/akkademy/AskTest.java
+++ learning-akka/akkademaid/src/test/java/com/akkademy/AskTest.java
@@ -18,26 +18,24 @@ import scala.concurrent.Await;
 import scala.concurrent.Future;
 
 public class AskTest {
-	
-	private final ActorSystem system = ActorSystem.create();
-    
-	private final Timeout timeout = new Timeout(10000, TimeUnit.MILLISECONDS);
 
-	private final TestProbe cacheProbe = new TestProbe(system);
-    
-	private final TestProbe httpClientProbe = new TestProbe(system);
-    
-	private final ActorRef articleParseActor = system.actorOf(Props.create(ParsingActor.class));
+    private final ActorSystem system = ActorSystem.create();
 
-	private final ActorRef askDemoActor = system.actorOf(Props.create(
-			AskDemoArticleParser.class, cacheProbe.ref().path().toString(), 
-			httpClientProbe.ref().path().toString(), articleParseActor.path().toString(), timeout)
-    );
+    private final Timeout timeout = new Timeout(10000, TimeUnit.MILLISECONDS);
 
+    private final TestProbe cacheProbe = new TestProbe(system);
+
+    private final TestProbe httpClientProbe = new TestProbe(system);
+
+    private final ActorRef articleParseActor = system.actorOf(Props.create(ParsingActor.class));
+
+    private final ActorRef askDemoActor = system.actorOf(Props.create(AskDemoArticleParser.class, cacheProbe.ref().path().toString(),
+            httpClientProbe.ref().path().toString(), articleParseActor.path().toString(), timeout));
 
     @Test
     public void itShouldParseArticleTest() throws Exception {
         Future<?> f = ask(askDemoActor, new ParseArticle(("http://www.google.com")), timeout);
+
         cacheProbe.expectMsgClass(GetRequest.class);
         cacheProbe.reply(new Status.Failure(new Exception("no cache")));
 
@@ -45,7 +43,7 @@ public class AskTest {
         httpClientProbe.reply(new HttpResponse(Articles.article1));
 
         String result = (String) Await.result(f, timeout.duration());
-        assert(result.contains("I’ve been writing a lot in emacs lately"));
-        assert(!result.contains("<body>"));
+        assert result.contains("I’ve been writing a lot in emacs lately");
+        assert !result.contains("<body>");
     }
 }
--- learning-akka/akkademaid/src/test/java/com/akkademy/TellTest.java
+++ learning-akka/akkademaid/src/test/java/com/akkademy/TellTest.java
@@ -18,24 +18,24 @@ import scala.concurrent.Await;
 import scala.concurrent.Future;
 
 public class TellTest {
-    ActorSystem system = ActorSystem.create("testSystem");
-    Timeout timeout = new Timeout(10000, TimeUnit.MILLISECONDS);
 
-    TestProbe cacheProbe = new TestProbe(system);
-    TestProbe httpClientProbe = new TestProbe(system);
-    ActorRef articleParseActor = system.actorOf(Props.create(ParsingActor.class));
+    private ActorSystem system = ActorSystem.create("testSystem");
 
-    ActorRef tellDemoActor = system.actorOf(
-            Props.create(TellDemoArticleParser.class,
-                    cacheProbe.ref().path().toString(),
-                    httpClientProbe.ref().path().toString(),
-                    articleParseActor.path().toString(),
-                    timeout)
-    );
+    private Timeout timeout = new Timeout(10000, TimeUnit.MILLISECONDS);
+
+    private TestProbe cacheProbe = new TestProbe(system);
+
+    private TestProbe httpClientProbe = new TestProbe(system);
+
+    private ActorRef articleParseActor = system.actorOf(Props.create(ParsingActor.class));
+
+    private ActorRef tellDemoActor = system.actorOf(Props.create(TellDemoArticleParser.class, cacheProbe.ref().path().toString(),
+            httpClientProbe.ref().path().toString(), articleParseActor.path().toString(), timeout));
 
     @Test
-    public void itShouldParseArticleTest() throws Exception{
+    public void itShouldParseArticleTest() throws Exception {
         Future<?> f = ask(tellDemoActor, new ParseArticle(("http://www.google.com")), timeout);
+
         cacheProbe.expectMsgClass(GetRequest.class);
         cacheProbe.reply(new Status.Failure(new Exception("no cache")));
 
@@ -43,9 +43,7 @@ public class TellTest {
         httpClientProbe.reply(new HttpResponse(Articles.article1));
 
         String result = (String) Await.result(f, timeout.duration());
-        assert(result.contains("I’ve been writing a lot in emacs lately"));
-        assert(!result.contains("<body>"));
+        assert result.contains("I’ve been writing a lot in emacs lately");
+        assert !result.contains("<body>");
     }
-
-
 }
