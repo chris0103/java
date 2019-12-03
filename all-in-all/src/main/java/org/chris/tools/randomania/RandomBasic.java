0565108b39c0b9d3a1c9fa9a7359d3b3da38d98e
czhu30 <CZhu30@its.jnj.com>
2019-12-03 13:55:02 +0800

    Update RandomBasic.java
    Reformatted.

--- all-in-all/src/main/java/org/chris/tools/randomania/RandomBasic.java
+++ all-in-all/src/main/java/org/chris/tools/randomania/RandomBasic.java
@@ -3,15 +3,15 @@ package org.chris.tools.randomania;
 import java.util.Random;
 
 public class RandomBasic {
-	
-	private static Random random = new Random();
 
-	public int randomNumber(int scope) {
-		return random.nextInt(scope) + 1;
-	}
-	
-	public static void main(String[] args) {
-		RandomBasic random = new RandomBasic();
-		System.out.println(random.randomNumber(476));
-	}
+    private static Random random = new Random();
+
+    public static void main(String[] args) {
+        RandomBasic random = new RandomBasic();
+        System.out.println(random.randomNumber(1196));
+    }
+
+    public int randomNumber(int scope) {
+        return random.nextInt(scope) + 1;
+    }
 }
