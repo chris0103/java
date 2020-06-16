e2affe164f7c9669972b4f40102fc47ba83bf61d
Youjing Zhu <chris0103@126.com>
2020-06-16 10:20:07 +0800

    Update BinaryTree.java
    Added BFS and DFS.

--- all-in-all/src/main/java/org/chris/study/algorithms/datastructure/BinaryTree.java
+++ all-in-all/src/main/java/org/chris/study/algorithms/datastructure/BinaryTree.java
@@ -1,17 +1,18 @@
 package org.chris.study.algorithms.datastructure;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class BinaryTree {
 	
 	private Node root;
 	
-	public BinaryTree(Object data) {
-		root = new Node(data);
+	public BinaryTree() {
 		
 		// test
-		root.setData(9);
+		root = new Node(9);
 		Node node1 = new Node(1);
 		Node node2 = new Node(6);
 		Node node3 = new Node(13);
@@ -43,6 +44,55 @@ public class BinaryTree {
 		
 	}
 	
+	public void breadthFirstSearch() {
+		List<Node> result = new ArrayList<>();
+		Queue<Node> queue = new LinkedList<>();
+		queue.add(root);
+		
+		while (!queue.isEmpty()) {
+			Node node = queue.poll();
+			result.add(node);
+			
+			if (node.getLeftChild() != null) {
+				queue.add(node.getLeftChild());
+			}
+			if (node.getRightChild() != null) {
+				queue.add(node.getRightChild());
+			}
+		}
+		
+		System.out.println("Breadth First Search: ");
+		for (Node node : result) {
+			System.out.print(node.getData() + " ");
+		}
+	}
+	
+	public void depthFirstSearch() {
+		List<Node> result = new ArrayList<>();
+		result.add(root);
+		result.addAll(depthFirstSearch(root.getLeftChild()));
+		result.addAll(depthFirstSearch(root.getRightChild()));
+		System.out.println("Depth First Search: ");
+		for (Node node : result) {
+			System.out.print(node.getData() + " ");
+		}
+	}
+	
+	private List<Node> depthFirstSearch(Node root) {
+		List<Node> result = new ArrayList<>();
+		if (root == null) {
+			return result;
+		}
+		result.add(root);
+		if (root.getLeftChild() != null) {
+			result.addAll(depthFirstSearch(root.getLeftChild()));
+		}
+		if (root.getRightChild() != null) {
+			result.addAll(depthFirstSearch(root.getRightChild()));
+		}
+		return result;
+	}
+	
 	public void findLongestPath() {
 		List<Node> path = new ArrayList<>();
 		path.add(root);
@@ -53,7 +103,7 @@ public class BinaryTree {
 		} else {
 			path.addAll(rightPath);
 		}
-		System.out.println("The logest path:");
+		System.out.println("The longest path:");
 		for (Node node : path) {
 			System.out.print(node.getData() + " ");
 		}
@@ -106,6 +156,7 @@ public class BinaryTree {
 			return data;
 		}
 
+		@SuppressWarnings("unused")
 		public void setData(Object data) {
 			this.data = data;
 		}
@@ -128,6 +179,6 @@ public class BinaryTree {
 	}
 	
 	public static void main(String[] args) {
-		new BinaryTree(9).findLongestPath();
+		new BinaryTree().depthFirstSearch();
 	}
 }
