08e5ecd124fe2f3d09226b507b2c0b9e7634f244
Youjing Zhu <chris0103@126.com>
2020-08-20 00:17:50 +0800

    Delete package-info.java

--- networking/src/main/java/brokenpipe/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * This package demonstrates the "Broken Pipe" error in networking programming.
- * <p>
- * Check the URL
- * <a href="https://www.cnblogs.com/metoy/p/6565486.html">https://www.cnblogs.com/metoy/p/6565486.html</a> for
- * reference.
- */
-
-package brokenpipe;
