93b8375cffd75a7b06e46f314210abe445095b37
Youjing Zhu <chris0103@126.com>
2023-03-29 16:31:04 +0800

    Update RandomPlus.java

--- all-in-all/src/main/java/org/chris/tools/randomania/RandomPlus.java
+++ all-in-all/src/main/java/org/chris/tools/randomania/RandomPlus.java
@@ -11,11 +11,13 @@ public class RandomPlus {
     public String randomWithWeight(List<String> items) {
         List<String> itemsWithWeight = new ArrayList<>();
         int size = items.size();
+        System.out.println("Item size: " + size);
         for (int i = 0; i < size; i++) {
             for (int j = size; j > i; j--) {
                 itemsWithWeight.add(items.get(i));
             }
         }
+        printItems(itemsWithWeight);
         return itemsWithWeight.get(random.nextInt(itemsWithWeight.size()));
     }
 
@@ -26,13 +28,42 @@ public class RandomPlus {
         }
         return randomWithWeight(items);
     }
+    
+    private void printItems(List<String> items) {
+    	for (String item : items) {
+    		System.out.println(item);
+    	}
+    }
 
     public static void main(String[] args) {
         List<String> items = new ArrayList<>();
-        items.add("1");
-        items.add("2");
-        items.add("3");
-        String randomItem = new RandomPlus().randomWithWeight(10);
-        System.out.println(randomItem);
+        items.add("Firefox Bookmarks");
+        items.add("Zhihu Fav");
+        items.add("Former Companies Arrangement");
+        items.add("Learning Arrange");
+        items.add("javalib");
+        items.add("Luke's Songslist");
+        items.add("Local -> Git");
+        items.add("Writing");
+        items.add("Duan She Li");
+        items.add("Pillow Books");
+        items.add("Reading");
+        items.add("Games");
+        items.add("New Songs");
+        
+        items.add("Computer Books scanner");
+        items.add("Move tools Classes from tools project to all-in-all project");
+        items.add("Practice");
+        items.add("English Listening");
+        items.add("手机容量清理");
+        items.add("Canon Research");
+        items.add("游戏整理");
+        items.add("To Be Enhanced Movie");
+        items.add("English Translation");
+        items.add("Deutsch");
+        items.add("Spider nets");
+        
+        String randomItem = new RandomPlus().randomWithWeight(items);
+        System.out.println("\nThe chosen one: " + randomItem);
     }
 }
