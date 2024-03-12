c8943f1f8a046eb3b1f9a6607cecbb4dbd3cf1ea
Youjing Zhu <chris0103@126.com>
2024-03-12 23:21:11 +0800

    Add the OnJava example projects.

---
+++ OnJava/src/chap03/objects/HelloDate.java
@@ -0,0 +1,11 @@
+package chap03.objects;
+
+import java.util.Date;
+
+public class HelloDate {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, it's: ");
+        System.out.println(new Date());
+    }
+}
