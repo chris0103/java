f357cfcd218b2959a3cae3a91969965e8f312647
Youjing Zhu <chris0103@126.com>
2024-03-20 21:48:21 +0800

    Add OnJava examples.

---
+++ OnJava/src/chap04/operators/AutoInc.java
@@ -0,0 +1,15 @@
+package chap04.operators;
+
+public class AutoInc {
+
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println("i: " + i);
+        System.out.println("++i: " + ++i); // Pre-increment
+        System.out.println("i++: " + i++); // Post-increment
+        System.out.println("i: " + i);
+        System.out.println("--i: " + --i); // Pre-decrement
+        System.out.println("i--: " + i--); // Post-decrement
+        System.out.println("i: " + i);
+    }
+}
