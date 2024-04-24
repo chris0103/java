1b1e8dceb38edf68a72447f2ca20a1ddb8f25d5c
Youjing Zhu <chris0103@126.com>
2024-04-24 11:02:18 +0800

    Create Overflow.java

---
+++ OnJava/src/chap04/operators/Overflow.java
@@ -0,0 +1,11 @@
+package chap04.operators;
+
+// Surprise! Java lets you overflow
+public class Overflow {
+    public static void main(String[] args) {
+        int big = Integer.MAX_VALUE;
+        System.out.println("big = " + big);
+        int bigger = big * 4;
+        System.out.println("bigger = " + bigger);
+    }
+}
