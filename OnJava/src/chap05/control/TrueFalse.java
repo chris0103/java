deb368c5f2c62556efc4810cfcb0d40d4fc7bf59
Youjing Zhu <chris0103@126.com>
2024-04-24 13:35:50 +0800

    Add OnJava examples.

---
+++ OnJava/src/chap05/control/TrueFalse.java
@@ -0,0 +1,9 @@
+package chap05.control;
+
+public class TrueFalse {
+
+    public static void main(String[] args) {
+        System.out.println(1 == 1);
+        System.out.println(1 == 2);
+    }
+}
