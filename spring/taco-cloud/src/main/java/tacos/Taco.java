be47f7aabc4687808d03d06c9fa75fb8693fdb4e
Youjing Zhu <chris0103@126.com>
2024-05-17 23:04:51 +0800

    Add sia examples.

---
+++ spring/taco-cloud/src/main/java/tacos/Taco.java
@@ -0,0 +1,23 @@
+package tacos;
+
+import java.util.List;
+
+public class Taco {
+
+    private String name;
+    private List<Ingredient> ingredients;
+
+    public Taco(String name, List<Ingredient> ingredients) {
+        this.name = name;
+        this.ingredients = ingredients;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+}
