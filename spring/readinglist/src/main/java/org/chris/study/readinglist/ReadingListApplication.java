5f7c8a24fa416f7ee88eae3650b0c3721e21d52d
Youjing Zhu <chris0103@126.com>
2024-06-16 22:52:49 +0800

    Added the readinglist application for Spring Boot examples.

---
+++ spring/readinglist/src/main/java/org/chris/study/readinglist/ReadingListApplication.java
@@ -0,0 +1,13 @@
+package org.chris.study.readinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingListApplication.class, args);
+	}
+
+}
---
+++ spring/readinglist/src/test/java/org/chris/study/readinglist/ReadingListApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chris.study.readinglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
