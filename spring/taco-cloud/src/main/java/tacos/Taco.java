bfd1943b854037b563e3fd97a8b2c3a5856143ef
Youjing Zhu <chris0103@126.com>
2024-07-19 23:37:02 +0800

    Update SIA examples.

--- spring/taco-cloud/src/main/java/tacos/Taco.java
+++ spring/taco-cloud/src/main/java/tacos/Taco.java
@@ -1,10 +1,18 @@
 package tacos;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import java.util.List;
 
 public class Taco {
 
+    @NotNull
+    @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
+
+    @NotNull
+    @Size(min=1, message = "You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
 
     public Taco() {
