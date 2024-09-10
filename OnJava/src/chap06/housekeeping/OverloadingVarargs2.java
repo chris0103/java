35505de6f66cdc0db48ad1d9b522ace43b33208f
Youjing Zhu <chris0103@126.com>
2024-09-10 22:28:53 +0800

    Added OnJava exmaples.

---
+++ OnJava/src/chap06/housekeeping/OverloadingVarargs2.java
@@ -0,0 +1,18 @@
+package chap06.housekeeping;
+
+// {WillNotCompile}
+public class OverloadingVarargs2 {
+
+    static void f(float i, Character... args) {
+        System.out.println("first");
+    }
+
+    static void f(char c, Character... args) {
+        System.out.println("second");
+    }
+
+    public static void main(String[] args) {
+        f(1, 'a');
+        f('a', 'b');
+    }
+}
---
+++ OnJava/src/chap06/housekeeping/OverloadingVarargs3.java
@@ -0,0 +1,17 @@
+package chap06.housekeeping;
+
+public class OverloadingVarargs3 {
+
+    static void f(float i, Character... args) {
+        System.out.println("first");
+    }
+
+    static void f(char c, Character... args) {
+        System.out.println("second");
+    }
+
+    public static void main(String[] args) {
+        f(1, 'a');
+        f('a', 'b');
+    }
+}
