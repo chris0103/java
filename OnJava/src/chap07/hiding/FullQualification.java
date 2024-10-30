8a6a378b0abc99e5bdc2747a65a99ad875911f94
Youjing Zhu <chris0103@126.com>
2024-10-30 21:38:33 +0800

    Add OnJava examples.

---
+++ OnJava/src/chap07/hiding/FullQualification.java
@@ -0,0 +1,8 @@
+package chap07.hiding;
+
+public class FullQualification {
+
+    public static void main(String[] args) {
+        java.util.ArrayList list = new java.util.ArrayList();
+    }
+}
---
+++ OnJava/src/chap07/hiding/SingleImport.java
@@ -0,0 +1,10 @@
+package chap07.hiding;
+
+import java.util.ArrayList;
+
+public class SingleImport {
+
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+    }
+}
