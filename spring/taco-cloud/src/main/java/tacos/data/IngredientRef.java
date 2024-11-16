82cfc19584fa36fbb598e8fde52a7d69d5cca74c
Youjing Zhu <chris0103@126.com>
2024-11-16 19:35:59 +0800

    Added SIA examples.

---
+++ spring/taco-cloud/src/main/java/tacos/data/IngredientRef.java
@@ -0,0 +1,15 @@
+package tacos.data;
+
+
+public class IngredientRef {
+
+    private final String ingredient;
+
+    public IngredientRef(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+}
--- spring/taco-cloud/src/main/resources/schema.sql
+++ spring/taco-cloud/src/main/resources/schema.sql
@@ -18,3 +18,18 @@ create table if not exists Taco (
   taco_order_key bigint not null,
   created_at timestamp not null
 );
+
+create table if not exists Ingredient_Ref (
+  ingredient varchar(4) not null,
+  taco bigint not null,
+  taco_key bigint not null
+);
+
+create table if not exists Ingredient (
+  id varchar(4) not null unique,
+  name varchar(25) not null,
+  type varchar(10) not null
+);
+
+alter table Taco add foreign key (taco_order) references Taco_Order(id);
+alter table Ingredient_Ref add foreign key (ingredient) references Ingredient(id);
