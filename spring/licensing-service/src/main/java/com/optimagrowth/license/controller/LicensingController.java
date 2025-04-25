eb155f8ed09f656923479573af371628064d0781
Youjing Zhu <chris0103@126.com>
2025-04-25 23:46:16 +0800

    Add SMIA examples.

---
+++ spring/licensing-service/src/main/java/com/optimagrowth/license/controller/LicensingController.java
@@ -0,0 +1,10 @@
+package com.optimagrowth.license.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/v1/organization/{organizationId}/license")
+public class LicensingController {
+
+}
---
+++ spring/licensing-service/src/main/java/com/optimagrowth/license/model/License.java
@@ -0,0 +1,61 @@
+package com.optimagrowth.license.model;
+
+public class License {
+
+    private int id;
+    private String licenseId;
+    private String organizationId;
+    private String productName;
+    private String licenseType;
+
+    public String getOrganizationId() {
+        return organizationId;
+    }
+
+    public void setOrganizationId(String organizationId) {
+        this.organizationId = organizationId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLicenseId() {
+        return licenseId;
+    }
+
+    public void setLicenseId(String licenseId) {
+        this.licenseId = licenseId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getLicenseType() {
+        return licenseType;
+    }
+
+    public void setLicenseType(String licenseType) {
+        this.licenseType = licenseType;
+    }
+
+    @Override
+    public String toString() {
+        return "License{" +
+                "id=" + id +
+                ", licenseId='" + licenseId + '\'' +
+                ", organizationId='" + organizationId + '\'' +
+                ", productName='" + productName + '\'' +
+                ", licenseType='" + licenseType + '\'' +
+                '}';
+    }
+}
