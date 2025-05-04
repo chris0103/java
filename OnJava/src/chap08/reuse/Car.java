9a7dd35bc36e69fe9950ea6f1a92a347f6c4ede5
Youjing Zhu <chris0103@126.com>
2025-05-04 23:13:49 +0800

    Added OnJava examples.

---
+++ OnJava/src/chap08/reuse/Car.java
@@ -0,0 +1,44 @@
+package chap08.reuse;
+
+class Engine {
+    public void start() {}
+    public void rev() {}
+    public void stop() {}
+}
+
+class Wheel {
+    public void inflate(int psi) {}
+}
+
+class Window {
+    public void rollup() {}
+    public void rolldown() {}
+}
+
+class Door {
+    public Window window = new Window();
+    public void open() {}
+    public void close() {}
+}
+
+// Composition with public objects
+public class Car {
+
+    public Engine engine = new Engine();
+    public Wheel[] wheels = new Wheel[4];
+    public Door
+        left = new Door(),
+        right = new Door();
+
+    public Car() {
+        for (int i = 0; i < 4; i++) {
+            wheels[i] = new Wheel();
+        }
+    }
+
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.left.window.rollup();
+        car.wheels[0].inflate(72);
+    }
+}
