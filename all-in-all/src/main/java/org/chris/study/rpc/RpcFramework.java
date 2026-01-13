8e1edea37bfa86762cf162f9749509082d9f24a4
Youjing Zhu <chris0103@126.com>
2026-01-13 23:32:34 +0800

    Updated RFC framework examples.

--- all-in-all/src/main/java/org/chris/study/rpc/RpcFramework.java
+++ all-in-all/src/main/java/org/chris/study/rpc/RpcFramework.java
@@ -1,12 +1,16 @@
 package org.chris.study.rpc;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * A demo PRC framework.
@@ -15,11 +19,8 @@ public class RpcFramework {
 
     /**
      * Exports the service.
-     * @param service
-     * @param port
-     * @throws Exception
      */
-    public static void export(final Object service, int port) throws Exception {
+    public static void export(final Object service, int port) throws IOException {
         if (service == null) {
             throw new IllegalArgumentException("service instance == null");
         }
@@ -27,55 +28,36 @@ public class RpcFramework {
             throw new IllegalArgumentException("Invalid port " + port);
         }
         System.out.println("Exporting service " + service.getClass().getName() + " on port " + port + "...");
-        ServerSocket server = new ServerSocket(port);
-        for(;;) {
-            try {
-                final Socket socket = server.accept();
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            try {
-                                ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
-                                try {
-                                    String methodName = input.readUTF();
-                                    Class<?>[] parameterTypes = (Class<?>[])input.readObject();
-                                    Object[] arguments = (Object[])input.readObject();
-                                    ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
-                                    try {
-                                        Method method = service.getClass().getMethod(methodName, parameterTypes);
-                                        Object result = method.invoke(service, arguments);
-                                        output.writeObject(result);
-                                    } catch (Throwable t) {
-                                        output.writeObject(t);
-                                    } finally {
-                                        output.close();
-                                    }
-                                } finally {
-                                    input.close();
-                                }
-                            } finally {
-                                socket.close();
-                            }
-                        } catch (Exception e) {
-                            e.printStackTrace();
+        try (ServerSocket server = new ServerSocket(port)) {
+            ExecutorService executor = Executors.newCachedThreadPool();
+            while (true) {
+                Socket socket = server.accept();
+                executor.submit(() -> {
+                    try (ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+                         ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream())) {
+                        String methodName = input.readUTF();
+                        Class<?>[] parameterTypes = (Class<?>[]) input.readObject();
+                        Object[] arguments = (Object[]) input.readObject();
+                        Method method = service.getClass().getMethod(methodName, parameterTypes);
+                        Object result = method.invoke(service, arguments);
+                        output.writeObject(result);
+                    } catch (Throwable t) {
+                        try (ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream())) {
+                            output.writeObject(t);
+                        } catch (Exception ex) {
+                            Logger.getLogger(RpcFramework.class.getName()).log(Level.SEVERE, "Error writing exception", ex);
                         }
                     }
-                }).start();
-            } catch (Exception e) {
-                e.printStackTrace();
+                });
             }
+
+        } catch (Exception e) {
+            Logger.getLogger(RpcFramework.class.getName()).log(Level.SEVERE, "Server error", e);
         }
     }
 
     /**
      * References the service.
-     * @param interfaceClass
-     * @param host
-     * @param port
-     * @return
-     * @param <T>
-     * @throws Exception
      */
     @SuppressWarnings("unchecked")
     public static <T> T refer(final Class<T> interfaceClass, final String host, final int port) throws Exception {
@@ -85,7 +67,7 @@ public class RpcFramework {
         if (! interfaceClass.isInterface()) {
             throw new IllegalArgumentException(interfaceClass.getName() + " must be interface class!");
         }
-        if (host == null || host.length() == 0) {
+        if (host == null || host.isEmpty()) {
             throw new IllegalArgumentException("Host == null!");
         }
         if (port <= 0 || port > 65535) {
@@ -94,32 +76,24 @@ public class RpcFramework {
         System.out.println(
                 "Referencing remote service " + interfaceClass.getName() + " from server " + host + ":" + port + "...");
         return (T) Proxy.newProxyInstance(
-                interfaceClass.getClassLoader(), new Class<?>[] {interfaceClass}, new InvocationHandler() {
-            public Object invoke(Object proxy, Method method, Object[] arguments) throws Throwable {
-                Socket socket = new Socket(host, port);
-                try {
-                    ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
-                    try {
+                interfaceClass.getClassLoader(), new Class<?>[] {interfaceClass}, (proxy, method, arguments) -> {
+                    try (Socket socket = new Socket(host, port);
+                         ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+                         ObjectInputStream input = new ObjectInputStream(socket.getInputStream())) {
+
                         output.writeUTF(method.getName());
                         output.writeObject(method.getParameterTypes());
                         output.writeObject(arguments);
-                        ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
-                        try {
-                            Object result = input.readObject();
-                            if (result instanceof Throwable) {
-                                throw (Throwable) result;
-                            }
-                            return result;
-                        } finally {
-                            input.close();
+
+                        Object result = input.readObject();
+                        if (result instanceof Throwable) {
+                            throw (Throwable) result;
                         }
-                    } finally {
-                        output.close();
+                        return result;
+                    } catch (Exception e) {
+                        Logger.getLogger(RpcFramework.class.getName()).log(Level.SEVERE, "Client error", e);
+                        throw e;
                     }
-                } finally {
-                    socket.close();
-                }
-            }
-        });
+                });
     }
 }
