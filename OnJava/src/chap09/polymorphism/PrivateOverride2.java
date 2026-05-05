6921012a665a18428223a221a9f5f050fbb3c6b4
Youjing Zhu <chris0103@126.com>
2026-05-06 00:17:27 +0800

    Updated OnJava examples.

---
+++ OnJava/src/chap09/polymorphism/PrivateOverride2.java
@@ -0,0 +1,22 @@
+package chap09.polymorphism;
+
+// Detecting a mistaken override using @Override
+public class PrivateOverride2 {
+
+    private void f() {
+        System.out.println("private f()");
+    }
+
+    public static void main(String[] args) {
+        PrivateOverride2 po = new Derived2();
+        po.f();
+    }
+}
+
+class Derived2 extends PrivateOverride2 {
+
+    //- @Override
+    public void f() {
+        System.out.println("public f()");
+    }
+}
